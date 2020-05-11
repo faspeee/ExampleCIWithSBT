378e543441b226154658f03e2b858e3af4a6626b
faspeeencina <fabian.aspeeencina@studio.unibo.it>
2020-05-11 23:16:36 +0200

    generic

--- src/main/scala/dbmanagment/GenericCrud.scala
+++ src/main/scala/dbmanagment/GenericCrud.scala
@@ -5,16 +5,15 @@ import dbmanagment.GiornoTable.GiornoTableRep
 import dbmanagment.ZonaTable.ZonaTableRep
 import dbmanagment.TurnoTable.TurnoTableRep
 import dbmanagment.implicitsGeneric.Brands
-import slick.jdbc.SQLServerProfile.api._
-import slick.lifted.AbstractTable
+import slick.lifted
+import slick.lifted.TableQuery
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.reflect.runtime.{universe => runtime}
 import scala.util.{Failure, Success}
 trait GenericCrudClass{
-  def create[F](element:F): Future[Int]
-  def read[F](element:F):Future[Option[Any]]
+  def insert[F](element:F): Future[Int]
+  def select[F](element:F):Future[Option[Any]]
   def update[F](element:F):Future[Int]
   def delete[F](element:F):Future[Int]
 }
@@ -25,35 +24,24 @@ object GenericCrudClass extends GenericCrudClass{
   def selectTableType[F,C<:GenericTable[F]:runtime.TypeTag]() =  {
     Brands[F,C]()
   }
-  override def create[F](element:F) =element match {
-    case Giorno(x,y,z)=>selectTableType[Giorno,GiornoTableRep]().create(Giorno(x,y,z))//.create(element.asInstanceOf)
-    case Zona(x,y)=>selectTableType[Zona,ZonaTableRep]().create(Zona(x,y))//.create(element)//.create(element.asInstanceOf)
-    case Turno(x,y,z)=>selectTableType[Turno,TurnoTableRep]().create(Turno(x,y,z))//.create(element)//.create(element.asInstanceOf)
+  override def insert[F](element:F) =element match {
+    case Giorno(x,y,z)=>selectTableType[Giorno,GiornoTableRep]().insert(Giorno(x,y,z))//.create(element.asInstanceOf)
+    case Zona(x,y)=>selectTableType[Zona,ZonaTableRep]().insert(Zona(x,y))//.create(element)//.create(element.asInstanceOf)
+    case Turno(x,y,z)=>selectTableType[Turno,TurnoTableRep]().insert(Turno(x,y,z))//.create(element)//.create(element.asInstanceOf)
   }
-  override def read[F](element:F)=element match {
-    case Giorno(_,_,z)=>selectTableType[Giorno,GiornoTableRep]().read(z.head)//.create(element.asInstanceOf)
-    case Zona(_,z)=>selectTableType[Zona,ZonaTableRep]().read(z.head)//.create(element)//.create(element.asInstanceOf)
-    case Turno(_,_,z)=>selectTableType[Turno,TurnoTableRep]().read(z.head)//.create(element)//.create(element.asInstanceOf)
+  override def select[F](element:F)=element match {
+    case Giorno(_,_,z)=>selectTableType[Giorno,GiornoTableRep]().select(z.head)//.create(element.asInstanceOf)
+    case Zona(_,z)=>selectTableType[Zona,ZonaTableRep]().select(z.head)//.create(element)//.create(element.asInstanceOf)
+    case Turno(_,_,z)=>selectTableType[Turno,TurnoTableRep]().select(z.head)//.create(element)//.create(element.asInstanceOf)
   }
   override def update[F](element:F)=element match {
-    case Giorno(x,y,z)=>selectTableType[Giorno,GiornoTableRep]().update(z.head,Giorno(x,y,z))//.create(element.asInstanceOf)
-    case Zona(x,y)=>selectTableType[Zona,ZonaTableRep]().update(y.head,Zona(x,y))//.create(element)//.create(element.asInstanceOf)
-    case Turno(x,y,z)=>selectTableType[Turno,TurnoTableRep]().update(z.head,Turno(x,y,z))//.create(element)//.create(element.asInstanceOf)
+    case Giorno(x,y,z)=>selectTableType[Giorno,GiornoTableRep]().update(Giorno(x,y,z))//.create(element.asInstanceOf)
+    case Zona(x,y)=>selectTableType[Zona,ZonaTableRep]().update(Zona(x,y))//.create(element)//.create(element.asInstanceOf)
+    case Turno(x,y,z)=>selectTableType[Turno,TurnoTableRep]().update(Turno(x,y,z))//.create(element)//.create(element.asInstanceOf)
   }
   override def delete[F](element:F)=element match {
     case Giorno(_,_,z)=>selectTableType[Giorno,GiornoTableRep]().delete(z.head)//.create(element.asInstanceOf)
     case Zona(_,z)=>selectTableType[Zona,ZonaTableRep]().delete(z.head)//.create(element)//.create(element.asInstanceOf)
     case Turno(_,_,z)=>selectTableType[Turno,TurnoTableRep]().delete(z.head)//.create(element)//.create(element.asInstanceOf)
   }
-}
-object tryobject extends App{
-
-  val r:GenericCrudClass = GenericCrudClass()
-  val s = Seq(Turno("22","Juanito"),Turno("22","Juanito"),Turno("22","Juanito"))
-  val ss = r.create(s)
-    ss.onComplete{
-      case Success(x) => println("inserite ", x)
-      case Failure(x) => println("DIO",x)
-    }
-  while(true){}
 }
--- src/main/scala/setting/InsertDataBase.scala
+++ src/main/scala/setting/InsertDataBase.scala
@@ -1,46 +1,36 @@
 package setting
+import dbmanagment.CaseClassDB.{Giorno, Zona}
 import dbmanagment.GenericCrudClass._
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 object InsertDataBase {
+  var id = 0
    def insertZona()= {
     try {
-     // implicit val implicitStringOrdering: GenericCRUD[Zona,ZonaTableRep] = Brands[ Zona,ZonaTableRep]()
-     //  val s = Zona("Giannis")
-      //  val f = implicitStringOrdering.create(s)
-      //   f onComplete { posts =>
-      //     for (post <- posts) println(post)
-      //   }
-      case class Zonas(s:String,ss:String,sss:Option[Int]=None)
-      val s = Zonas("Giannis","2")
-      create(s)
-      insertGiorno()
-     /* Await.result(db.run(DBIO.seq(
-        zone += ZonaSet("Gianni"),
-        zone += ZonaSet("Fabian"),
-        zone += ZonaSet("Francesco")
-      )).flatMap { _ =>
-        //#readall
-        // Read all coffees and print them to the console
-        println("Zona:")
-
-        db.run(zone.result).map(_.foreach(t =>
-          println("  " + t.IdZone + "\t" + t.Zone)
-        ))
-      }, Duration.Inf)
-*/
-    // Equivalent SQL code:
-    // select COF_NAME, SUP_ID, PRICE, SALES, TOTAL from COFFEES
-    //#readall
-
-   } finally  println("insertZona:")
+      val s = Zona("Giannis")
+      insert(s) onComplete { posts =>
+        for (post <- posts) {id = post;println(post)}
+      }
+   }
   }
-  def insertGiorno(): Unit ={
-
-  //  implicit val implicitStringOrdering2: GenericCRUD[Giorno,GiornoTableRep] = Brands[ Giorno,GiornoTableRep]()
-   // val ss = Giorno(11,"Lunedi")
-   // val ff = implicitStringOrdering2.create(ss)
-   // ff onComplete { posts =>
-   //   for (post <- posts) println(post)
-   // }
+  def selectZona(): Unit ={
+    val s = Zona("",Some(id))
+     select(s)  onComplete { posts =>
+       for (post <- posts) println(post)
+     }
+  }
+  def updateZona(): Unit ={
+    val s = Zona("el mejor es Fabian :)",Some(id))
+    update(s).onComplete {
+      case Success(x) => println(x)
+      case Failure(x) => println(x)
+    }
+  }
+  def deleteZona(): Unit ={
+    val s = Zona("",Some(id))
+    delete(s)  onComplete { posts =>
+      for (post <- posts) println(post)
+    }
   }
 }
