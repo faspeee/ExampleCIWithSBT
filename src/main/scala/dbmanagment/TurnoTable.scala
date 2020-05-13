d4ddf4f2dcd6c6a9f49b2c95f85ebd9203133354
fra.cassano2 <francesco.cassano2@studio.unibo.it>
2020-05-13 18:08:36 +0200

    fra fa casino

--- src/main/scala/dbmanagment/TurnoTable.scala
+++ src/main/scala/dbmanagment/TurnoTable.scala
@@ -3,16 +3,8 @@ import slick.jdbc.SQLServerProfile.api._
 import scala.language.postfixOps
 import CaseClassDB._
 object TurnoTable{
-<<<<<<< HEAD
 
-  private case class Turno(nomeTurno: String, fasciaOraria: String,id: Option[Int] = None)
-
-  private class TurnoTableRep(tag: Tag) extends Table[Turno](tag, "TurnoSets") {
-    def id = column[Int]("idTurno",O.PrimaryKey,O.AutoInc)
-
-=======
   class TurnoTableRep(tag: Tag) extends GenericTable[Turno](tag, "TurnoSets","IdTurno") {
->>>>>>> 4bd9665747cee51aec02e0b140e0db110b69d136
     def nomeTurno = column[String]("NomeTurno")
     def fasciaOraria = column[String]("FasciaOraria")
     def * = (nomeTurno, fasciaOraria,id.?).mapTo[Turno]
