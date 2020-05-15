180caa08d32b6f73cfa314ae3e7b880cf3ef9094
fra.cassano2 <francesco.cassano2@studio.unibo.it>
2020-05-16 01:36:20 +0200

    http akka

--- src/main/scala/MainFra.scala
+++ src/main/scala/MainFra.scala
@@ -3,7 +3,7 @@ import controller.FraController
 import javafx.application.Application
 import javafx.stage.Stage
 import model.Model
-import view.MainView
+//import view.MainView
 
 
 object MainFra extends App {
